602fe529fcdc1329eb61cd9881d0405d1b606228
Philip Nelson <philipnelson5@gmail.com>
2017-06-07 14:52:23 -0600

    solved 22

--- 022/main.cpp
+++ 022/main.cpp
@@ -1,39 +1,49 @@
 #include <algorithm>
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <string>
 #include <vector>
 
 namespace
 {
-	std::vector<std::string> names;
+  std::vector<std::string> names;
 }
 
 int score(std::string name)
 {
-
+  auto sum = 0;
+  for (auto c : name) sum += c - 'A' + 1;
+  return sum;
 }
 
 int main()
 {
-	std::ifstream fin("names.txt");
-	if(!fin) {std::cout << "BAD FILE!" << std::endl; return EXIT_FAILURE;}
+  std::ifstream fin("names.txt");
+  if (!fin)
+  {
+    std::cout << "BAD FILE!" << std::endl;
+    return EXIT_FAILURE;
+  }
 
-	std::string file;
-	fin >> file;
+  std::string strnames;
+  fin >> strnames;
 
-	bool newWord = false;
-	std::string nextWord = "";
-	for(unsigned int i = 0; i < file.size(); ++i)
-	{
-		if(file[i] == "\"")
-			
-			file[i] != ",")
-	}
+  auto beg = strnames.find("\"");
+  auto end = strnames.find("\"", beg + 1);
 
-		names.push_back(name);
+  while (beg != std::string::npos)
+  {
+    names.emplace_back(strnames.begin() + beg + 1, strnames.begin() + end);
+    beg = strnames.find("\"", end + 1);
+    end = strnames.find("\"", beg + 1);
+  }
 
-	std::sort(names.begin(), names.end(), [](std::string a, std::string b){return a < b;});
+  std::sort(names.begin(), names.end(), [](std::string a, std::string b) {
+    return a < b;
+  });
 
-	std::cout << "First names " << names[0] << std::endl << "END" << std::endl;
+  int scores = 0;
+  for (int i = 0; i < names.size(); ++i) scores += score(names[i]) * (i+1);
+
+  std::cout << scores << std::endl;
 }
